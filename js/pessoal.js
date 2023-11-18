aeaec1684e78628681a7d69b6423e25e42aa911d
gilbran-ifrn <122621985+gilbran-ifrn@users.noreply.github.com>
2023-11-18 19:08:31 -0300

    Atualização 1.01
    - Inserção do desenvolvedor
    - Remoção da exibição da cabeça da tabela

---
+++ js/pessoal.js
@@ -0,0 +1,126 @@
+function calculaLPO() {
+    var corrida = (document.getElementById('corrida').value).split(":");
+    var lpo = parseFloat(document.getElementById('lpo').value);
+
+    var tabela = document.getElementById('tabelaLPO').getElementsByTagName('tbody')[0];
+
+    var mostrarTabela = document.querySelectorAll('[id^="tabelaLPO"]');
+    mostrarTabela.forEach(function(element) {
+        element.removeAttribute("style");
+    });
+
+    while (tabela.rows.length > 0) {
+        tabela.deleteRow(0);
+    }
+
+    var titulos = ["120%", "115%", "110%", "105%", "100%", "95%", "90%", "85%", "80%", "75%", "70%", "65%", "60%", "55%", "50%", "45%", "40%"];
+
+    /* LPO */
+
+    var porcentagem = 1.2;
+
+    for (var i = 0; i < titulos.length; i++) {
+        // insere linha
+        var linha = tabela.insertRow(-1);
+
+        // insere coluna
+        var col1 = linha.insertCell(0);
+        var col2 = linha.insertCell(1);
+        var col3 = linha.insertCell(2);
+
+        col1.innerHTML = titulos[i];
+        
+        col2.innerHTML = (lpo * porcentagem).toFixed(0) + " kg"; // .toFixed controla as casas decimais
+
+        col3.innerHTML = (lpo * porcentagem * 2.20462262185).toFixed(0) + " lb";
+
+        porcentagem -= 0.05;
+    }
+}
+
+
+
+
+function calculaCorrida() {
+    var corrida = (document.getElementById('corrida').value).split(":");
+    var lpo = parseFloat(document.getElementById('lpo').value);
+
+    var tabela = document.getElementById('tabelaCorrida').getElementsByTagName('tbody')[0];
+
+    var mostrarTabela = document.querySelectorAll('[id^="tabelaCorrida"]');
+    mostrarTabela.forEach(function(element) {
+        element.removeAttribute("style");
+    });
+
+    while (tabela.rows.length > 0) {
+        tabela.deleteRow(0);
+    }
+
+    var titulos = ["120%", "115%", "110%", "105%", "100%", "95%", "90%", "85%", "80%", "75%", "70%", "65%", "60%", "55%", "50%", "45%", "40%"];
+
+    /* Corrida */
+    var porcentagem = 0.8;
+
+
+    var min = parseInt(corrida[0]);
+    var seg = parseInt(corrida[1]);
+    tempoTotal = (min * 60) + seg;
+
+    for (var i = 0; i < titulos.length; i++) {
+        // insere linha
+        var linha = tabela.insertRow(-1);
+
+        // insere coluna
+        var col1 = linha.insertCell(0);
+        var col2 = linha.insertCell(1);
+        var col3 = linha.insertCell(2);
+
+        col1.innerHTML = titulos[i];
+
+        novoTempo = tempoTotal * porcentagem;
+        min = Math.floor(novoTempo / 60);
+        seg = novoTempo - (min * 60);
+        
+
+        col2.innerHTML = min.toFixed(0) + ":" + seg.toFixed(0).padStart(2, '0') + " min/km";
+
+        col3.innerHTML = (60 / (novoTempo/60)).toFixed(1) + " km/h";
+
+        porcentagem += 0.05;
+    }
+    }
+
+    function changeActive(clickedElement, elementToShow) {
+    var paginationItems = document.querySelectorAll('#paginacao .page-item');
+
+    paginationItems.forEach(function(item) {
+        item.classList.remove('active');
+    });
+
+    var parentLi = clickedElement.parentElement;
+    parentLi.classList.add('active');
+
+    if (elementToShow == 'formCorrida'){
+        var elementsToHide = document.querySelectorAll('[id^="formLPO"]');
+        var elementToDisplay = document.getElementById(elementToShow);
+        elementsToHide.forEach(function(element) {
+            element.style.display = 'none';
+        });
+
+        
+        if (elementToDisplay) {
+            elementToDisplay.style.display = 'block';
+        }
+    } else {
+        var elementsToHide = document.querySelectorAll('[id^="formCorrida"]');
+        var elementToDisplay = document.getElementById(elementToShow);
+        elementsToHide.forEach(function(element) {
+            element.style.display = 'none';
+        });
+
+        
+        if (elementToDisplay) {
+            elementToDisplay.style.display = 'block';
+        }
+    }
+}
