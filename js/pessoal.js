c638ddc05c4a3f556c90f904dde2a293392c41f9
gilbran-ifrn <122621985+gilbran-ifrn@users.noreply.github.com>
2023-11-18 22:57:49 -0300

    Atualização 1.02
    - badge com outline
    - cor de hover da tabela azul
    - validacao basica do formulario

--- js/pessoal.js
+++ js/pessoal.js
@@ -1,96 +1,110 @@
-function calculaLPO() {
-    var corrida = (document.getElementById('corrida').value).split(":");
-    var lpo = parseFloat(document.getElementById('lpo').value);
-
-    var tabela = document.getElementById('tabelaLPO').getElementsByTagName('tbody')[0];
-
-    var mostrarTabela = document.querySelectorAll('[id^="tabelaLPO"]');
-    mostrarTabela.forEach(function(element) {
-        element.removeAttribute("style");
-    });
-
-    while (tabela.rows.length > 0) {
-        tabela.deleteRow(0);
+function handleFormSubmission(event) {
+    event.preventDefault(); // Prevent form submission if validation fails
+    if (this.checkValidity() === false) {
+        event.stopPropagation(); // Stop the event from propagating further
     }
+    this.classList.add("was-validated"); // Add 'was-validated' class to enable Bootstrap's styling
+}
 
-    var titulos = ["120%", "115%", "110%", "105%", "100%", "95%", "90%", "85%", "80%", "75%", "70%", "65%", "60%", "55%", "50%", "45%", "40%"];
+function calculaLPO() {
+    var calcLPO = document.getElementById("calcLPO");
+    calcLPO.addEventListener("submit", handleFormSubmission, false)
 
-    /* LPO */
+    if (lpo = parseFloat(document.getElementById('lpo').value)){
+        var tabela = document.getElementById('tabelaLPO').getElementsByTagName('tbody')[0];
 
-    var porcentagem = 1.2;
+        var mostrarTabela = document.querySelectorAll('[id^="tabelaLPO"]');
+        mostrarTabela.forEach(function(element) {
+            element.removeAttribute("style");
+        });
 
-    for (var i = 0; i < titulos.length; i++) {
-        // insere linha
-        var linha = tabela.insertRow(-1);
+        while (tabela.rows.length > 0) {
+            tabela.deleteRow(0);
+        }
 
-        // insere coluna
-        var col1 = linha.insertCell(0);
-        var col2 = linha.insertCell(1);
-        var col3 = linha.insertCell(2);
+        var titulos = ["120%", "115%", "110%", "105%", "100%", "95%", "90%", "85%", "80%", "75%", "70%", "65%", "60%", "55%", "50%", "45%", "40%"];
 
-        col1.innerHTML = titulos[i];
-        
-        col2.innerHTML = (lpo * porcentagem).toFixed(0) + " kg"; // .toFixed controla as casas decimais
+        /* LPO */
 
-        col3.innerHTML = (lpo * porcentagem * 2.20462262185).toFixed(0) + " lb";
+        var porcentagem = 1.2;
 
-        porcentagem -= 0.05;
+        for (var i = 0; i < titulos.length; i++) {
+            // insere linha
+            var linha = tabela.insertRow(-1);
+
+            // insere coluna
+            var col1 = linha.insertCell(0);
+            var col2 = linha.insertCell(1);
+            var col3 = linha.insertCell(2);
+
+            col1.innerHTML = titulos[i];
+            
+            col2.innerHTML = (lpo * porcentagem).toFixed(0) + " kg"; // .toFixed controla as casas decimais
+
+            col3.innerHTML = (lpo * porcentagem * 2.20462262185).toFixed(0) + " lb";
+
+            porcentagem -= 0.05;
+        }
     }
 }
 
-
-
-
 function calculaCorrida() {
-    var corrida = (document.getElementById('corrida').value).split(":");
-    var lpo = parseFloat(document.getElementById('lpo').value);
+    var calcLPO = document.getElementById("calcCorrida");
+    calcLPO.addEventListener("submit", handleFormSubmission, false)
 
-    var tabela = document.getElementById('tabelaCorrida').getElementsByTagName('tbody')[0];
-
-    var mostrarTabela = document.querySelectorAll('[id^="tabelaCorrida"]');
-    mostrarTabela.forEach(function(element) {
-        element.removeAttribute("style");
-    });
-
-    while (tabela.rows.length > 0) {
-        tabela.deleteRow(0);
-    }
-
-    var titulos = ["120%", "115%", "110%", "105%", "100%", "95%", "90%", "85%", "80%", "75%", "70%", "65%", "60%", "55%", "50%", "45%", "40%"];
-
-    /* Corrida */
-    var porcentagem = 0.8;
-
-
-    var min = parseInt(corrida[0]);
-    var seg = parseInt(corrida[1]);
-    tempoTotal = (min * 60) + seg;
-
-    for (var i = 0; i < titulos.length; i++) {
-        // insere linha
-        var linha = tabela.insertRow(-1);
-
-        // insere coluna
-        var col1 = linha.insertCell(0);
-        var col2 = linha.insertCell(1);
-        var col3 = linha.insertCell(2);
-
-        col1.innerHTML = titulos[i];
-
-        novoTempo = tempoTotal * porcentagem;
-        min = Math.floor(novoTempo / 60);
-        seg = novoTempo - (min * 60);
+    if (corr = (document.getElementById('corrida').value)){
+        if (corr.indexOf(":") != -1){
         
+            var corrida = corr.split(":");
 
-        col2.innerHTML = min.toFixed(0) + ":" + seg.toFixed(0).padStart(2, '0') + " min/km";
+            var tabela = document.getElementById('tabelaCorrida').getElementsByTagName('tbody')[0];
 
-        col3.innerHTML = (60 / (novoTempo/60)).toFixed(1) + " km/h";
+            var mostrarTabela = document.querySelectorAll('[id^="tabelaCorrida"]');
+            mostrarTabela.forEach(function(element) {
+                element.removeAttribute("style");
+            });
 
-        porcentagem += 0.05;
+            while (tabela.rows.length > 0) {
+                tabela.deleteRow(0);
+            }
+
+            var titulos = ["120%", "115%", "110%", "105%", "100%", "95%", "90%", "85%", "80%", "75%", "70%", "65%", "60%", "55%", "50%", "45%", "40%"];
+
+            /* Corrida */
+            var porcentagem = 0.8;
+
+
+            var min = parseInt(corrida[0]);
+            var seg = parseInt(corrida[1]);
+            tempoTotal = (min * 60) + seg;
+
+            for (var i = 0; i < titulos.length; i++) {
+                // insere linha
+                var linha = tabela.insertRow(-1);
+
+                // insere coluna
+                var col1 = linha.insertCell(0);
+                var col2 = linha.insertCell(1);
+                var col3 = linha.insertCell(2);
+
+                col1.innerHTML = titulos[i];
+
+                novoTempo = tempoTotal * porcentagem;
+                min = Math.floor(novoTempo / 60);
+                seg = novoTempo - (min * 60);
+                
+
+                col2.innerHTML = min.toFixed(0) + ":" + seg.toFixed(0).padStart(2, '0') + " min/km";
+
+                col3.innerHTML = (60 / (novoTempo/60)).toFixed(1) + " km/h";
+
+                porcentagem += 0.05;
+            }
+        }
     }
-    }
+}
 
-    function changeActive(clickedElement, elementToShow) {
+function changeActive(clickedElement, elementToShow) {
     var paginationItems = document.querySelectorAll('#paginacao .page-item');
 
     paginationItems.forEach(function(item) {
